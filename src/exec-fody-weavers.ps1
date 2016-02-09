6e72c7a9f08e18b3780fa6c038101c379eddc9aa
Warty <ItzWarty@gmail.com>
2016-02-08 20:39:54 -0800

    Add gitignore, rename to exec-fody-weavers

--- src/exec-fody-weavers.ps1
+++ src/exec-fody-weavers.ps1
@@ -1,6 +1,6 @@
 ﻿# Load additional assemblies needed by script_inner, then exec it.
 Add-Type -AssemblyName Microsoft.Build.Framework;
-$output = &("$PSScriptRoot/script_inner.ps1");
+$output = &("$PSScriptRoot/exec-fody-weavers-inner.ps1");
 if ($output[$output.Length - 1] -ne '0') {
   Write-Host("Inner script returned nonzero result.");
   exit 1;
